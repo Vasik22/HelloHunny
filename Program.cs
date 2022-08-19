6315af8acb18996d8be43c612480f9119312041d
Your Name <you@example.com>
2022-08-19 18:09:45 +0300

    Hello_user

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username == "Вероника")
+{
+    Console.WriteLine("Ура, ура, наконец-то!");
+}
+else
+{
+    Console.WriteLine("Привет,!");
+    Console.WriteLine(username);
+
+}
